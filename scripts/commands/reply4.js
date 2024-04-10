72623b13bff28f3d64d6a75006d0f4f1480cfeda
MHIMRAN25 <147571069+MHIMRAN25@users.noreply.github.com>
2024-04-11 00:25:07 +0600

    Update reply4.js
    Signed-off-by: MHIMRAN25 <147571069+MHIMRAN25@users.noreply.github.com>

--- scripts/commands/reply4.js
+++ scripts/commands/reply4.js
@@ -35,22 +35,3 @@ react.includes("ভুল সালাম দিলে কি হয়")) {
 	module.exports.run = function({ api, event, client, __GLOBAL }) {
 
                }
-
-module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
-	var { threadID, messageID } = event;
-	let react = event.body.toLowerCase();
-if (react.includes("tahole salam kivabe dite hoy") ||
-     react.includes("তাহলে সালাম কিভাবে দেয়") || 
-react.includes("তুই সালাম দে তো") || react.includes("tui salam de to") || react.includes("salam kaiche deti he") || react.includes("salam kemne dey")) {
-		var msg = {
-				body: "~ ❌ One word wrong means change of meaning. So please give salam correctly➡️Arabic- السلام عليكم و الرحمة✅ ا"
-     body: ❌ One word wrong means change of meaning. So please give salam correctly➡️বাংলা - আসসালামু আলাইকুম ওয়া রহমাতুল্লাহি✅"
-    body:body:"English - assalamu alaikum wa Rahmatullah✅
-			}
-			api.sendMessage(msg, threadID, messageID);
-    api.setMessageReaction("✅", event.messageID, (err) => {}, true)
-		}
-	}
-	module.exports.run = function({ api, event, client, __GLOBAL }) {
-
-               }
