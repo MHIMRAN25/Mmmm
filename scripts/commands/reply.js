06be09da424f105208dd5c83a74d524ade0f0ccf
MHIMRAN25 <147571069+MHIMRAN25@users.noreply.github.com>
2024-04-10 17:17:07 +0600

    Update reply.js
    Signed-off-by: MHIMRAN25 <147571069+MHIMRAN25@users.noreply.github.com>

--- scripts/commands/reply.js
+++ scripts/commands/reply.js
@@ -18,7 +18,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	let react = event.body.toLowerCase();
 	if(react.includes("ঈদুল ফিতর") ||
      react.includes("eid") || 
-react.includes("hi") || react.includes("ईद मुबारक") || react.includes("이드 무바라크") || react.includes("عيد مبارك") ||
+react.includes("happy") || react.includes("ईद मुबारक") || react.includes("이드 무바라크") || react.includes("عيد مبارك") ||
 react.includes("hlw") ||
 react.includes("ঈদ মোবারক") || react.includes("Īda mōbāraka") || react.includes("🫡") ||
 react.includes("মোবারক") ||
@@ -28,7 +28,7 @@ react.includes("Eid") ||
 react.includes("eid Mobarak") ||
 react.includes("🌜")) {
 		var msg = {
-				body: "~অগ্রিম ঈদ মোবারক🌜🤗!!🌚"
+				body: "~ ঈদ মোবারক🌜🤗!!🌚"
 			}
 			api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("🌜", event.messageID, (err) => {}, true)
@@ -38,10 +38,4 @@ react.includes("🌜")) {
 
                }
 
-	if (react.includes("আসসালামু আলাইকুম")==0 || react.includes("असलमु अलैकुम")==0 || react.includes("Asslamu alaikum")==0 || react.includes("السلام عليكم ")==0 || react.includes("아살라무 알라이쿰")==0 || react.includes("আসসালামু আলাইকুম ওয়া রহমাতুল্লাহি")==0 || react.includes("সালামের উত্তর")==0 || react.includes("アッサラーム・アライクム")==0 || react.includes("阿萨拉穆·阿拉库姆")==0 && !bot.includes(event.senderID))  {
-		var msg = {
-				body: "وعليكم السلام و الرحمة وبركاته🥰"
-			}
-			api.sendMessage(msg, threadID, messageID);
-    api.setMessageReaction("🥰", event.messageID, (err) => {}, true)
-	   }
+	
