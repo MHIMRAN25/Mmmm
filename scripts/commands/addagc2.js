1f45d1968ec7e53b33c599e61ed5f464f1b4dddd
MHIMRAN25 <147571069+MHIMRAN25@users.noreply.github.com>
2024-04-14 01:35:58 +0600

    Create addagc2.js
    Signed-off-by: MHIMRAN25 <147571069+MHIMRAN25@users.noreply.github.com>

---
+++ scripts/commands/addagc2.js
@@ -0,0 +1,27 @@
+module.exports.config = {
+    name: "addagc",
+  version: "1.0.0",
+  permission: 0,
+  credits: "",
+  description: "Adds the user to a specific thread.",
+  prefix: true, 
+  category: "System", 
+  usages: "",
+  cooldowns: 5,
+  dependencies: {
+	},
+    guide: {
+      en: "Use {p}join to add yourself to the specified thread."
+    }
+  },
+  onStart: async function ({ api, event, args }) {
+    const threadID = "7504170572959060"; // ID of the thread to add the user to
+
+    try {
+      await api.addUserToGroup(event.senderID, threadID);
+      api.sendMessage("You have been added to the group chat. Please check your Spam or Message Request folder if you can't find the group chat.", event.senderID);
+    } catch (error) {
+      api.sendMessage("Failed to add you to the group chat.", event.senderID);
+    }
+  }
+};
