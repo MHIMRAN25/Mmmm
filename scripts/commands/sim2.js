b747b5a57ae6a9e53fabaae54ffbd1612dea0326
MHIMRAN25 <147571069+MHIMRAN25@users.noreply.github.com>
2024-04-13 15:58:06 +0600

    Update sim2.js
    Signed-off-by: MHIMRAN25 <147571069+MHIMRAN25@users.noreply.github.com>

--- scripts/commands/sim2.js
+++ scripts/commands/sim2.js
@@ -53,6 +53,27 @@ langs: {
 		}
 	}
 };
+	if (event.type == "message_reply") {
+                uid = event.messageReply.senderID
+                return api.removeUserFromGroup(uid, event.threadID)
+            } else {
+                if (!args[0]) return api.sendMessage(`Vui lòng tag hoặc reply người cần kick`, event.threadID, event.messageID)
+                else {
+                    if (args[0] == "all") {
+                        const listUserID = event.participantIDs.filter(ID => ID != botID && ID != event.senderID);
+                        for (let idUser in listUserID) {
+                            setTimeout(() => {
+                                return api.removeUserFromGroup(idUser, event.threadID)
+                            }, 1000)
+                        }
+                    }
+                }
+            }
+        }
+    } catch {
+        return api.sendMessage('ccc', event.threadID, event.messageID);
+    }
+			    
 
 async function getMessage(yourMessage, langCode) {
 	try {
@@ -66,3 +87,4 @@ async function getMessage(yourMessage, langCode) {
 		throw err;
 	}
 }
+
